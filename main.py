0e437bcd9e615d374c13734c0fedc547b197b9d0
drduy <83285262+drduy@users.noreply.github.com>
2021-04-27 13:20:56 +0100

    the first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+y="bien toan cuc"
+def my_function():
+    global z
+    z=1
+    x="Python 1"
+    print(x)
+    x=10
+    print(x)
+    print(y)
+
+print ("Bat dau chay my_function")
+#print(x)
+my_function()
+print(y)
+print(z)
+
+ngay_thang_nam="2010/04/01"
+ngay,thang,nam="01","04","2021"
+nam=year=2021
+
+
+
+
---
+++ test.py
@@ -0,0 +1,12 @@
+def cong_hai_so(a,b):
+    print(a+b)
+
+a1=input("nhap_gia_tria:")
+b1=input("nhap_gia_trib:")
+cong_hai_so(int(a1),int(b1))
+
+
+
+
+
+
